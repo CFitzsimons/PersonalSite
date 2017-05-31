fdb8c8347c892f9ec35ea88b902908bf2149bef0
CFitzsimons <colin.fitzsimons3@mail.dcu.ie>
2017-05-31 13:35:13 +0100

    Added personal description

--- src/routes/dashboard/index.jsx
+++ src/routes/dashboard/index.jsx
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
+import { Card, CardActions, CardHeader, CardTitle, CardText } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
 import ProfilePhoto from './colin.jpg';
+
 
 const style = {
   container: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    flexDirection: 'column',
+    flexDirection: 'row',
   },
-  image: {
-    borderRadius: 25,
-    maxWidth: '50%',
-    width: '100%',
-    height: 'auto',
-    marginTop: 35,
-    boxShadow: '10px 10px 20px black',
+  paperContainer: {
+    margin: 10,
+    height: 250,
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  textContainer: {
+    flex: 0.7,
   },
 };
+
+const introText = 'I am a PhD Student funded by the Irish Research Council conducting' +
+  'research into program construction in immersive environments.' +
+  'A great deal of my current work revolves around investigating' +
+  'the cognitive benefits of an immersive language as well as novel' +
+  'approaches to designing immersive programming systems.';
 
 export default class Dashboard extends Component {
   componentDidMount() {
@@ -26,7 +37,21 @@ export default class Dashboard extends Component {
   render() {
     return (
       <div style={style.container}>
-        <img style={style.image} alt="Colin Fitzsimons" src={ProfilePhoto} />
+        <Card style={{margin: 10}}>
+          <CardHeader
+            title="Colin Fitzsimons"
+            subtitle="PhD Student"
+            avatar={ProfilePhoto}
+          />
+          <CardTitle title="Department of Computer Science" subtitle="Maynooth University" />
+          <CardText>
+            {introText}
+          </CardText>
+          <CardActions>
+            <FlatButton label="LinkedIn" onTouchTap={() => window.open('https://linkedin.com/in/cfitzsimons', '_blank')} />
+            <FlatButton label="Github" onTouchTap={() => window.open('https://github.com/cfitzsimons', '_blank')} />
+          </CardActions>
+        </Card>
       </div>
     );
   }
