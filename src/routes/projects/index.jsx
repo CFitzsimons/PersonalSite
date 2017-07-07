e5139a0d13a992376afaf284e7dd19c3f8450bb7
CFitzsimons <colin.fitzsimons3@mail.dcu.ie>
2017-07-07 14:48:10 +0100

    Removed some junk from projects

--- src/routes/projects/index.jsx
+++ src/routes/projects/index.jsx
@@ -3,26 +3,26 @@ import React, { Component } from 'react';
 // import GridIcon from './gridicon';
 // import {GridList, GridTile} from 'material-ui/GridList';
 
-const style = {
-  gridList: {
-    height: 450,
-    overflowY: 'auto',
-  },
-  item: {
-    alignSelf: 'center',
-    width: 150,
-    height: 150,
-    backgroundColor: 'pink',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderStyle: 'solid',
-    borderWidth: 5,
-    borderColor: 'blue',
-    borderRadius: 150,
-    margin: 15,
-  },
-};
+// const style = {
+//   gridList: {
+//     height: 450,
+//     overflowY: 'auto',
+//   },
+//   item: {
+//     alignSelf: 'center',
+//     width: 150,
+//     height: 150,
+//     backgroundColor: 'pink',
+//     display: 'flex',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     borderStyle: 'solid',
+//     borderWidth: 5,
+//     borderColor: 'blue',
+//     borderRadius: 150,
+//     margin: 15,
+//   },
+// };
 
 
 export default class Projects extends Component {
