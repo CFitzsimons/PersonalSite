889fc23bbead47be170b9b273880791bca1bdc20
CFitzsimons <colin.fitzsimons3@mail.dcu.ie>
2017-07-07 16:19:35 +0100

    Beging rethemeing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { createMuiTheme, createPalette } from 'material-ui/styles';
+
+// const theme = createMuiTheme({
+//   primary: {
+//     500: 'orange',
+//   },
+// });
+
+const theme = createMuiTheme({
+  palette: createPalette({
+    primary: {
+      500: 'orange'
+    }
+  }),
+});
 
 import Routes from './routes/index';
 
@@ -11,7 +26,7 @@ require('./index.css');
 injectTapEventPlugin();
 
 const Main = () => (
-  <MuiThemeProvider>
+  <MuiThemeProvider theme={theme}>
     <Routes />
   </MuiThemeProvider>
 );
